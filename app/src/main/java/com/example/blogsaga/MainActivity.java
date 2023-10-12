5271e6106dacf5ff04fe5b453086f6a6f1b8e4f3
Suraj18-das <dsuraj2004@gmail.com>
2023-10-12 11:03:15 +0530

    hello chck

--- app/src/main/java/com/example/blogsaga/MainActivity.java
+++ app/src/main/java/com/example/blogsaga/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addFragment(new SignupFragment());
+        ///hello check
 
     }
 
